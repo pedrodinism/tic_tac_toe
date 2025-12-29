491164b74186faa68c004855c6b70dd2f0110775
pedrodinism <pedrodinisma@proton.me>
2025-12-29 19:28:45 +0000

    added calculateWinner

--- script.js
+++ script.js
@@ -34,8 +34,21 @@ function GameEngine () {
         lastPlayer = player
     }
 
+    const calculateWinner = () => {
+        const winningCombos = [[1,2,3], [4,5,6], [7,8,9], // rows
+                               [1,4,7], [2,5,8], [3,6,9], // columns
+                               [1,5,9], [3,5,7]] // diagonals
+        for (let combo of winningCombos) {
+            const [a,b,c] = combo
+            if (gameState[a] != 0 && gameState[a] === gameState[b] && gameState[a] === gameState[c]) {
+                return gameState[a]
+            }
+        }
+    }
+
     return {
         play,
-        gameState
+        gameState,
+        calculateWinner
     }
 }
