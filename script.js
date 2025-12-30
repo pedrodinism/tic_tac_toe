5bcc1e1da061b8a20958c9d151b8ac10a46744fa
pedrodinism <pedrodinisma@proton.me>
2025-12-30 18:34:31 +0000

    started sketching the UI | created the Scoreboard module | created the Board module

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Game engine module
+
 function GameEngine () {
     const gameState = {
         1: 0,
@@ -46,9 +48,74 @@ function GameEngine () {
         }
     }
 
+    const getGameState = () => {
+        return gameState
+    }
+
     return {
         play,
-        gameState,
+        getGameState,
         calculateWinner
+    }
+}
+
+// Scoreboard module
+
+function Scoreboard () {
+    const score = {
+        "X": 0,
+        "O": 0 
+    }
+
+    const resetScore = () => {
+        score["X"] = 0
+        score["O"] = 0
+    }
+
+    const addPoint = (player) => {
+        if (player != "X" && player != "O") {
+            console.log("Invalid player " + player)
+            return
+        }
+        if (player = "X") {
+            score["X"]++
+        }
+        if (player = "O") {
+            score["O"]++
+        }
+    }
+
+    const getPlayerScore = (player) => {
+        if (player != "X" && player != "O") {
+            console.log("Invalid player " + player)
+            return
+        }
+        return score[player]
+    }
+
+    return {
+        resetScore,
+        addPoint,
+        getPlayerScore
+    }
+}
+
+function Board (gameEngine) {
+    const boardgame = document.querySelector("#board")
+    const renderBoard = () => {
+        const state = gameEngine.getGameState()
+        for (let i = 1; i < 10; i++) {
+            const value = state[i]
+            if (value === 0) {
+                return
+            }
+            const id = "#cell-" + i
+            const cell = document.querySelector(id)
+            cell.textContent = value
+        }
+    }
+
+    return {
+        renderBoard
     }
 }
